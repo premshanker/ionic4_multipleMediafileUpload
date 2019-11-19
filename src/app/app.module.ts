ddedf39847f7ef03ac059a23ef0b202bf48e28a9
premshanker <prem2bajpayee@gmail.com>
2019-11-19 22:56:28 +0530

    First Commit

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,22 +1,37 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { RouteReuseStrategy } from '@angular/router';
-
+ 
 import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
 import { SplashScreen } from '@ionic-native/splash-screen/ngx';
 import { StatusBar } from '@ionic-native/status-bar/ngx';
-
+ 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { ImagePicker } from '@ionic-native/image-picker/ngx';
+import { File } from '@ionic-native/File/ngx';
+import { MediaCapture } from '@ionic-native/media-capture/ngx';
+import { Media } from '@ionic-native/media/ngx';
+import { StreamingMedia } from '@ionic-native/streaming-media/ngx';
+import { PhotoViewer } from '@ionic-native/photo-viewer/ngx';
+import { HttpClientModule } from '@angular/common/http';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
 
 @NgModule({
   declarations: [AppComponent],
   entryComponents: [],
-  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule, HttpClientModule],
   providers: [
     StatusBar,
     SplashScreen,
-    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy }
+    { provide: RouteReuseStrategy, useClass: IonicRouteStrategy },
+    ImagePicker,
+    MediaCapture,
+    File,
+    Media,
+    StreamingMedia,
+    PhotoViewer,
+    WebView
   ],
   bootstrap: [AppComponent]
 })
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,258 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import { ImagePicker } from '@ionic-native/image-picker/ngx';
+import { ActionSheetController, Platform, ToastController, LoadingController } from '@ionic/angular';
+import {
+  MediaCapture,
+  MediaFile,
+  CaptureError
+} from '@ionic-native/media-capture/ngx';
+import { File, FileEntry } from '@ionic-native/File/ngx';
+import { HttpClient } from '@angular/common/http';
+import { WebView } from '@ionic-native/ionic-webview/ngx';
+import { Media, MediaObject } from '@ionic-native/media/ngx';
+import { StreamingMedia } from '@ionic-native/streaming-media/ngx';
+import { PhotoViewer } from '@ionic-native/photo-viewer/ngx';
+import { finalize } from 'rxjs/operators';
+ 
+const MEDIA_FOLDER_NAME = 'my_media';
+ 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
-  styleUrls: ['home.page.scss'],
+  styleUrls: ['home.page.scss']
 })
-export class HomePage {
+export class HomePage implements OnInit {
+   isMedia:boolean = false;
+  files = [];
+  
+ 
+  constructor(
+    private imagePicker: ImagePicker,
+    private mediaCapture: MediaCapture,
+    private file: File,
+    private media: Media,
+    private streamingMedia: StreamingMedia,
+    private photoViewer: PhotoViewer,
+    private actionSheetController: ActionSheetController,
+    private plt: Platform,
+    private http: HttpClient,
+    private webview: WebView,
+    private toastController: ToastController,
+    private loadingController: LoadingController
+  ) {}
+ 
+  ngOnInit() {
+    this.plt.ready().then(() => {
+      let path = this.file.dataDirectory;
+      this.file.checkDir(path, MEDIA_FOLDER_NAME).then(
+        () => {
+          
+          this.loadFiles();
+          
+        },
+        err => {
+          this.file.createDir(path, MEDIA_FOLDER_NAME, false);
+        }
+      );
+    });
+  }
+ 
+  loadFiles() {
+    this.file.listDir(this.file.dataDirectory, MEDIA_FOLDER_NAME).then(
+      res => {
+        this.files = res;
+        console.log("this.files.length " + this.files.length);
+          if(this.files.length>1){
+            this.isMedia = true;
+          }else{
+            this.isMedia = false;
+          }
+        console.log(res)
+      },
+      err => console.log('error loading files: ', err)
+    );
+  }
+  async selectMedia() {
+    const actionSheet = await this.actionSheetController.create({
+      header: 'What would you like to add?',
+      buttons: [
+        {
+          text: 'Capture Image',
+          handler: () => {
+            this.captureImage();
+          }
+        },
+        {
+          text: 'Record Video',
+          handler: () => {
+            this.recordVideo();
+          }
+        },
+        {
+          text: 'Record Audio',
+          handler: () => {
+            this.recordAudio();
+          }
+        },
+        {
+          text: 'Load multiple',
+          handler: () => {
+            this.pickImages();
+          }
+        },
+        {
+          text: 'Cancel',
+          role: 'cancel'
+        }
+      ]
+    });
+    await actionSheet.present();
+  }
+ 
+  pickImages() {
+    this.imagePicker.getPictures({}).then(
+      results => {
+        for (var i = 0; i < results.length; i++) {
+          this.copyFileToLocalDir(results[i]);
+        }
+      }
+    );
+ 
+    // If you get problems on Android, try to ask for Permission first
+    // this.imagePicker.requestReadPermission().then(result => {
+    //   console.log('requestReadPermission: ', result);
+    //   this.selectMultiple();
+    // });
+  }
+ 
+  captureImage() {
+    this.mediaCapture.captureImage().then(
+      (data: MediaFile[]) => {
+        if (data.length > 0) {
+          this.copyFileToLocalDir(data[0].fullPath);
+        }
+      },
+      (err: CaptureError) => console.error(err)
+    );
+  }
+ 
+  recordAudio() {
+    this.mediaCapture.captureAudio().then(
+      (data: MediaFile[]) => {
+        if (data.length > 0) {
+          this.copyFileToLocalDir(data[0].fullPath);
+        }
+      },
+      (err: CaptureError) => console.error(err)
+    );
+  }
+ 
+  recordVideo() {
+    this.mediaCapture.captureVideo().then(
+      (data: MediaFile[]) => {
+        if (data.length > 0) {
+          this.copyFileToLocalDir(data[0].fullPath);
+        }
+      },
+      (err: CaptureError) => console.error(err)
+    );
+  }
+  copyFileToLocalDir(fullPath) {
+    let myPath = fullPath;
+    // Make sure we copy from the right location
+    if (fullPath.indexOf('file://') < 0) {
+      myPath = 'file://' + fullPath;
+    }
+ 
+    const ext = myPath.split('.').pop();
+    const d = Date.now();
+    const newName = `${d}.${ext}`;
+ 
+    const name = myPath.substr(myPath.lastIndexOf('/') + 1);
+    const copyFrom = myPath.substr(0, myPath.lastIndexOf('/') + 1);
+    const copyTo = this.file.dataDirectory + MEDIA_FOLDER_NAME;
+ 
+    this.file.copyFile(copyFrom, name, copyTo, newName).then(
+      success => {
+        this.loadFiles();
+      },
+      error => {
+        console.log('error: ', error);
+      }
+    );
+  }
+ 
+  openFile(f: FileEntry) {
+    if (f.name.indexOf('.wav') > -1) {
+      // We need to remove file:/// from the path for the audio plugin to work
+      const path =  f.nativeURL.replace(/^file:\/\//, '');
+      const audioFile: MediaObject = this.media.create(path);
+      audioFile.play();
+    } else if (f.name.indexOf('.MOV') > -1 || f.name.indexOf('.mp4') > -1) {
+      // E.g: Use the Streaming Media plugin to play a video
+      this.streamingMedia.playVideo(f.nativeURL);
+    } else if (f.name.indexOf('.jpg') > -1) {
+      // E.g: Use the Photoviewer to present an Image
+      this.photoViewer.show(f.nativeURL, 'MY awesome image');
+    }
+  }
+  async presentToast(text) {
+    const toast = await this.toastController.create({
+        message: text,
+        position: 'bottom',
+        duration: 3000
+    });
+    toast.present();
+  }
+  deleteFile(f: FileEntry) {
+    const path = f.nativeURL.substr(0, f.nativeURL.lastIndexOf('/') + 1);
+    this.file.removeFile(path, f.name).then(() => {
+      this.loadFiles();
+    }, err => console.log('error remove: ', err));
+  }
 
-  constructor() {}
-
+  startUpload(imgEntry) {
+    this.file.resolveLocalFilesystemUrl(imgEntry.nativeURL)
+        .then(entry => {
+            ( < FileEntry > entry).file(file => this.readFile(file))
+        })
+        .catch(err => {
+            this.presentToast('Error while reading file.');
+        });
+}
+ 
+readFile(file: any) {
+    const reader = new FileReader();
+    reader.onloadend = () => {
+        const formData = new FormData();
+        const imgBlob = new Blob([reader.result], {
+            type: file.type
+        });
+        formData.append('file', imgBlob, file.name);
+        this.uploadImageData(formData);
+    };
+    reader.readAsArrayBuffer(file);
+}
+ 
+async uploadImageData(formData: FormData) {
+    const loading = await this.loadingController.create({
+        message: 'Uploading image...',
+    });
+    await loading.present();
+ 
+    this.http.post("http://103.133.215.102/~aagamits/test2/upload.php", formData)
+        .pipe(
+            finalize(() => {
+                loading.dismiss();
+            })
+        )
+        .subscribe(res => {
+            if (res['success']) {
+                this.presentToast('File upload complete.')
+            } else {
+                this.presentToast('File upload failed.')
+            }
+        });
+      }
+        
 }
